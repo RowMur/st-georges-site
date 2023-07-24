b217635e5719a51adbf3b5d870763df67af52065
RowMur <rowanomurray@gmail.com>
2023-07-24 22:37:59 +0100

    Only show scroll bar if needed

--- src/components/competitionDivision/index.tsx
+++ src/components/competitionDivision/index.tsx
@@ -11,16 +11,16 @@ interface CompetitionDivisionProps {
 const CompetitionDivision = ({ id }: CompetitionDivisionProps) => {
   return (
     <div className="flex flex-col w-full md:grid grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-4">
-      <div className="col-span-2 max-h-[30rem] overflow-y-scroll">
+      <div className="col-span-2 max-h-[30rem] overflow-y-auto">
         <DivisionStandings id={id} />
       </div>
-      <div className="col-span-1 max-h-[30rem] overflow-y-scroll">
+      <div className="col-span-1 max-h-[30rem] overflow-y-auto">
         <DivisionPotms id={id} />
       </div>
-      <div className="col-span-1 max-h-[30rem] overflow-y-scroll">
+      <div className="col-span-1 max-h-[30rem] overflow-y-auto">
         <DivisionDoublesAverages id={id} />
       </div>
-      <div className="col-span-2 max-h-[30rem] overflow-y-scroll">
+      <div className="col-span-2 max-h-[30rem] overflow-y-auto">
         <DivisionAverages id={id} />
       </div>
     </div>
